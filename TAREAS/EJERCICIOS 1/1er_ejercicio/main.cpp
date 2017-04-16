7fa4ead089477829c4163d6f4bc2c341eb2dabfa
Gustavo Ñaupa Canaza <gustavonaupacanaza@MacBook-Pro-de-Gustavo.local>
2017-04-16 16:34:55 -0500

    haciendo tarea

---
+++ TAREAS/EJERCICIOS 1/1er_ejercicio/main.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+//Escriba una programa en C++ que convierta de millas a kilómetros. Su programa debe solicitar por teclado el ingreso de las millas.
+
+int main ()
+{
+    int n;
+    cout<<"Cuantas millas desea convertir a kilometros : ";
+    cin>>n;
+    cout<<"Usted tiene : "<<n*(1.61)<<" kilometros"<<endl;
+}
+
