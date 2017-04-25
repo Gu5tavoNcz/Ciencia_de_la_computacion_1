3ac3bfbdd32f721a1271a0881b26a5d69ac863e4
ALUMNO-A <ALUMNO-A@UCSP.AULAB>
2017-04-25 10:14:10 -0500

    11vo actualizado

--- TAREAS/EJERCICIOS 1/11vo_ejercicio/main.cpp
+++ TAREAS/EJERCICIOS 1/11vo_ejercicio/main.cpp
@@ -2,11 +2,11 @@
 
 using namespace std;
 
-int llenar_matri(int mat[2][2])
+int llenar_matri(int mat[3][3])
 {
-    for(int i=0;i<2;i++)
+    for(int i=0;i<3;i++)
     {
-        for (int j=0;j<2;j++)
+        for (int j=0;j<3;j++)
         {
             cout<<"Ingrese el valor de la posicion M["<<i<<"]["<<j<<"] ";
             cin>>mat[i][j];
@@ -14,26 +14,26 @@ int llenar_matri(int mat[2][2])
     }
 }
 
-void imprimir_matri(int m[2][2])
+void imprimir_matri(int m[3][3])
 {
-    for(int i=0;i<2;i++)
+    for(int i=0;i<3;i++)
     {
-        for (int j=0;j<2;j++)
+        for (int j=0;j<3;j++)
         {
             cout<<m[i][j]<<",";
         }
     }
 }
 
-void multi_m(int a[2][2],int b[2][2])
+void multi_m(int a[3][3],int b[3][3])
 {
-    int c[2][2];
-    for(int i=0;i<2;i++)
+    int c[3][3];
+    for(int i=0;i<3;i++)
     {
-        for (int j=0;j<2;j++)
+        for (int j=0;j<3;j++)
         {
             c[i][j]=0;
-            for(int k=0;k<2;k++)
+            for(int k=0;k<3;k++)
             {
                 c[i][j]=c[i][j]+a[i][k]*b[k][j];
             }
@@ -46,14 +46,12 @@ void multi_m(int a[2][2],int b[2][2])
 
 int main()
 {
-    int matriz_a[2][2];
+    int matriz_a[3][3];
     cout<<"llene su matriz A"<<endl;
     llenar_matri(matriz_a);
-    int matriz_b[2][2];
+    int matriz_b[3][3];
     cout<<"llene su matriz B"<<endl;
     llenar_matri(matriz_b);
-    cout<<endl<<"La multipiclacion de la matriz A y B es :"<<endl;
+    cout<<endl<<"La multipiclacion de la matriz A y B es :"<<endl<<endl;
     multi_m(matriz_a,matriz_b);
-
-
 }
