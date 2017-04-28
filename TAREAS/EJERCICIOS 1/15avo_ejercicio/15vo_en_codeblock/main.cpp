57b5c97870d12f703dd3105c7b4e6e6e52dd6c31
ALUMNO-G <ALUMNO-G@UCSP.AULAB>
2017-04-28 10:31:27 -0500

    Agregando en codeblock

---
+++ TAREAS/EJERCICIOS 1/15avo_ejercicio/15vo_en_codeblock/main.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include <cstdlib>
+
+using namespace std;
+
+/*
+Escriba una función que evalue una cadena de caracteres y retorne true
+si la cadena representa un palindrome y false en caso contrario.
+ */
+
+bool palindromo(char palabra[],int ini,int fin)
+{
+    if(ini>=fin)
+        return true;
+    if(palabra[ini]==palabra[fin])
+        return palindromo(palabra,ini+1,fin-1);
+    else
+        return false;
+}
+
+int main()
+{
+    char cad[50];
+    cout<<"Ingrese una palabra: ";
+    cin.getline(cad,50);
+    bool resultado;
+    resultado=palindromo(cad,0,14);
+    if(resultado==true)
+        cout<<"Si es palindroma";
+    else
+        cout<<"No es palindroma";
+}
