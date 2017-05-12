feb95fbe66aa886e0de557b877a10d31f6aff48d
ALUMNO-G <ALUMNO-G@UCSP.AULAB>
2017-05-12 10:29:12 -0500

    9no proyecto con clases y vectores

---
+++ LABORATORIO/9vo_proyecto/include/Point.h
@@ -0,0 +1,12 @@
+#ifndef POINT_H
+#define POINT_H
+
+
+class Point
+{
+    public:
+        double x,y;
+        void print();
+};
+
+#endif // POINT_H
---
+++ LABORATORIO/9vo_proyecto/include/Vector.h
@@ -0,0 +1,12 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+#include "Point.h"
+
+class Vector
+{
+    public:
+        Point start,fin;
+        void print();
+};
+
+#endif // VECTOR_H
