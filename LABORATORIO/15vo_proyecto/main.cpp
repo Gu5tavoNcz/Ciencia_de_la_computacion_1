24ba3f8c583dd3787cac81be30b93a190df4b251
Gu5tavoNcz <gustavo_ncz93@outlook.com>
2017-06-02 10:55:27 -0500

    Update main.cpp

--- LABORATORIO/15vo_proyecto/main.cpp
+++ LABORATORIO/15vo_proyecto/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Avion.h"
+#include "Torre.h"
 #include "Avion_privado.h"
 #include "avion_comercial.h"
 
@@ -7,7 +8,12 @@ using namespace std;
 
 int main()
 {
+    Torre control;
     avion_comercial av_c1;
     avion_comercial av_c2;
     avion_comercial av_c3;
+    control.A.push_back(&av_c1);
+    control.A.push_back(&av_c2);
+    control.A.push_back(&av_c3);
+    
 }
