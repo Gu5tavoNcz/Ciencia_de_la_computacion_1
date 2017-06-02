ce4847d63acb61765003a632dd7bb1afe38f6353
Gu5tavoNcz <gustavo_ncz93@outlook.com>
2017-06-02 10:29:39 -0500

    Update Conserje.cpp

--- LABORATORIO/14vo_proyecto/src/Conserje.cpp
+++ LABORATORIO/14vo_proyecto/src/Conserje.cpp
@@ -3,6 +3,10 @@
 Conserje::Conserje(string miGenero,string miNombre,Cumpleanhos miCumple,string miTurno,double miSueldo)
 :Persona(miGenero,miNombre,miCumple),turno(miTurno),sueldo(miSueldo){}
 
+void Conserje::mostrar()
+{
+    cout<<"Genero: "<<genero<<"\nNombre: "<<nombre<<"\nTurno: "<<turno<<"\nSueldo: "<<sueldo<<endl;
+}
 /*
 Conserje::~Conserje()
 {
