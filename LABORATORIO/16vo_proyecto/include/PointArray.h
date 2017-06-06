6a65703f9d3efb48763384f7cda158d3fd84e5e0
Gu5tavoNcz <gustavo_ncz93@outlook.com>
2017-06-06 02:38:02 -0500

    actualizando

--- LABORATORIO/16vo_proyecto/include/PointArray.h
+++ LABORATORIO/16vo_proyecto/include/PointArray.h
@@ -6,11 +6,12 @@ class PointArray
 {
     int tamano;
     Point *puntos;
-    void redimensionar(int tamano);
+    void redimensionar(int miTamano);
 public:
     PointArray();
-    PointArray(Point misPuntos[],int tamano);
+    PointArray(Point misPuntos[],int miTamano);
     PointArray(PointArray &p);
+    ~PointArray();
 };
 
 #endif // POINTARRAY_H
--- LABORATORIO/16vo_proyecto/src/PointArray.cpp
+++ LABORATORIO/16vo_proyecto/src/PointArray.cpp
@@ -1,8 +1,37 @@
 #include "PointArray.h"
 
+PointArray::redimensionar(int miTamano)
+{
+
+}
+
 PointArray::PointArray()
 {
     tamano=0;
     puntos=new Point[0];
 }
 
+PointArray::PointArray(Point misPuntos[],int miTamano)
+{
+    tamano=miTamano;
+    puntos=new Point[miTamano];
+    for(int i=0;i<miTamano;i++)
+    {
+        puntos[i]=misPuntos[i];
+    }
+}
+
+PointArray::PointArray(PointArray &p)
+{
+    tamano=p.tamano;
+    puntos=new Point[tamano];
+    for(int i=0;i<tamano;i++)
+    {
+        puntos[i]=p.puntos[i];
+    }
+}
+
+PointArray::~PointArray()
+{
+    delete[] puntos;
+}
