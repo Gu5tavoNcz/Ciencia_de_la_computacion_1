496f747eb1bdcdc3cfe6cd0d75592a9bca01167e
Gu5tavoNcz <gustavo_ncz93@outlook.com>
2017-06-08 15:41:45 -0500

    Mapa casi terminado

--- TAREAS/Proyecto_final/Pacman_1.0/main.cpp
+++ TAREAS/Proyecto_final/Pacman_1.0/main.cpp
@@ -6,40 +6,39 @@ using namespace std;
 #define MAXFILAS 30
 #define MAXCOLUMNAS 51
 BITMAP *buffer;
-BITMAP *muro_o;
-BITMAP *muro_v;
-BITMAP *muro_ai,*muro_ad,*muro_bi,*muro_bd,*muro_a,*muro_b,*muro_i,*muro_d;
+BITMAP *roca;
+BITMAP *muro_o,*muro_v,*muro_ai,*muro_ad,*muro_bi,*muro_bd,*muro_a,*muro_b,*muro_i,*muro_d;
 
 char mapa[MAXFILAS][MAXCOLUMNAS]={
     "(----------------------)  (----------------------)",
     "|                      |  |                      |",
-    "|                      |  |                      |",
-    "|                      [--]                      |",
+    "| (------d a (-------) |  | (-------) a i------) |",
+    "| |        | |   a   | [--] |   a   | |        | |",
+    "| | i-d id | [-d | i-]      [-d | i-] | id i-d | |",
+    "| |        |     |              |     |        | |",
+    "| [------d b i-d | i----------d | i-d b i------] |",
+    "|                |              |                |",
+    "[-----)   i----d b i-d i-d i-d  b i----d   (-----]",
+    "      | a                                a |      ",
+    "      | | a     i----------------d     a | |      ",
+    "      | | |                            | | |      ",
+    "------] | | (-) XXXXXXX    XXXXXXX (-) | | [------",
+    "        b b | | X                X | | b b        ",
+    "--------d   b b X                X b b   i--------",
+    "        a a     X                X     a a        ",
+    "------) | | a a X                X a a | | (------",
+    "      | | b | b XXXXXXXXXXXXXXXXXX b | b | |      ",
+    "      | |   |                        |   | |      ",
+    "      | b X b (--d a (-d  i-) a i--) b X b |      ",
+    "(-----]       |    b |      | b    |       [-----)",
+    "|        (----]      |      |      [----)        |",
+    "|        |      (-d  (------)  i-)      |        |",
+    "[----) a | i--d |   (]      [)   | i--d | a i----]",
+    "(----] | |      | a |  (--)  | a |      | | (----)",
+    "|      b b i----] | | (]  [) | | [----d b b [d   |",
+    "| a               | | |    | | |               a |",
+    "| [-----d i-d i-d b b [d  i] b b i-d i-d i-----] |",
     "|                                                |",
-    "|                                                |",
-    "|                                                |",
-    "|                                                |",
-    "|                                                |",
-    "[-----)                                    (-----]",
-    "      |                                    |      ",
-    "      |                                    |      ",
-    "      |                                    |      ",
-    "------]                                    [------",
-    "                                                  ",
-    "------)                                    (------",
-    "      |                                    |      ",
-    "      |                                    |      ",
-    "      |                                    |      ",
-    "(-----]                                    [-----)",
-    "| XXXoXX XXXXXXXXXXX XXoXXX XX XXXXXXXXXX XXoXXX |",
-    "|oXXXoXX oo |ooo|ooo XXoXXXoXoo |ooo|ooo XXoXXXo |",
-    "| XXXoXXXXXX XXX XXXXXXoXXX XXXXX XXX XXXXXXoXXX |",
-    "|     XX     XXX     XX     X XX  XXX     XX     |",
-    "| XXXoXX XXXXXXXXXXX XXoXXX XXXXXXXXXX    XXoXXX |",
-    "|oXXX| o| o o o o o |o |XXXoX o    o o o |o |XXXo|",
-    "| XXXoXXXX XXXXXXXX XXX XXX XXXXX XXXXXX XXX XXX |",
-    "|oXXXoXXXX          XXX XXXoX            XXX XXXo|",
-    "|  o |o o  XXXXXXXX o o| o  X o  XXXXXXX o o| o  |",
     "[------------------------------------------------]",
 };
 
@@ -81,43 +80,14 @@ void dibujar_mapa()
             case 'd':
                 draw_sprite(buffer,muro_d,j*20,i*20);
                 break;
+            case 'X':
+                draw_sprite(buffer,roca,j*20,i*20);
+                break;
             }
         }
     }
 }
-/*
-void dibujar_mapa()
-{
-    for(int i=0;i<MAXFILAS;i++){
-        for(int j=0;j<MAXCOLUMNAS;j++)
-        {
-            if(mapa[i][j]=='-')
-            {
-                draw_sprite(buffer,muro_o,j*20,i*20);
-            }
-            if(mapa[i][j]=='|')
-            {
-                draw_sprite(buffer,muro_v,j*20,i*20);
-            }if(mapa[i][j]=='|')
-            {
-                draw_sprite(buffer,muro_v,j*20,i*20);
-            }
-            if(mapa[i][j]=='|')
-            {
-                draw_sprite(buffer,muro_v,j*20,i*20);
-            }
-            if(mapa[i][j]=='|')
-            {
-                draw_sprite(buffer,muro_v,j*20,i*20);
-            }
-            if(mapa[i][j]=='|')
-            {
-                draw_sprite(buffer,muro_v,j*20,i*20);
-            }
-        }
-    }
-}
-*/
+
 void pantalla()
 {
     blit(buffer,screen,0,0,0,0,1000,600);
@@ -142,6 +112,7 @@ int main()
     muro_b=load_bitmap("muro_b.bmp",NULL);
     muro_i=load_bitmap("muro_i.bmp",NULL);
     muro_d=load_bitmap("muro_d.bmp",NULL);
+    roca=load_bitmap("roca_20.bmp",NULL);
     while(!key[KEY_ESC])
     {
         dibujar_mapa();
